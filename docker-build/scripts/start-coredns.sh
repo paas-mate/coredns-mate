bdab52cdeed9a219c354b7cdeca450e3a954ecbf
goflutterjava <goflutterjava@gmail.com>
2022-10-24 17:22:03 +0800

    coredns support delve

--- docker-build/scripts/start-coredns.sh
+++ docker-build/scripts/start-coredns.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-coredns -conf=/opt/coredns/Corefile
+
+if [ -z "$COREDNS_DELVE_DEBUG" ] || [ $COREDNS_DELVE_DEBUG == "false" ]; then
+    coredns -conf=/opt/coredns/Corefile
+else
+    nohup dlv --listen=:2345 --headless=true --api-version=2 exec coredns -conf=/opt/coredns/Corefile
+fi
