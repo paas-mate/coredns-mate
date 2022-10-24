25d8223e63896df8cb1dd5a2c2a2e4db99efb7e2
Lico-Tom <583566138@qq.com>
2022-10-24 16:26:03 +0800

    add COREDNS_DELVE_DEBUG.

--- docker-build/scripts/start-coredns.sh
+++ docker-build/scripts/start-coredns.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-coredns -conf=/opt/coredns/Corefile
+rmem-size=""
+wmem-size=""
+if [ -n "$RMEM_SIZE" ]; then
+    rmem-size="-rmem=udp#'${RMEM_SIZE}'"
+fi
+if [ -n "$WMEM_SIZE" ]; then
+    wmem-size="-wmem=udp#'${WMEM_SIZE}'"
+fi
+if [ -z "$COREDNS_DELVE_DEBUG" ] || [ $COREDNS_DELVE_DEBUG == "false" ]; then
+    coredns -conf=/opt/coredns/Corefile ${rmem-size} ${wmem-size}
+else
+    nohup dlv --listen=:2345 --headless=true --api-version=2 exec coredns -conf=/opt/coredns/Corefile ${rmem-size} ${wmem-size}
+fi
+
